596c41a8be1a59b9e0112e9c7877c975b8ff407c
Raul Venegas <rvenegas199@gmail.com>
2022-07-29 15:40:55 -0400

    creacion del test 5 y 6

---
+++ src/test/java/tests/Tests5.java
@@ -0,0 +1,67 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import pages.CreacionCuenta;
+import pages.HomePage;
+import pages.LoginPage;
+import utils.DataDriven;
+import utils.PropertiesDriven;
+
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class Tests5 {
+    //Atributos
+    private WebDriver driver;
+    private HomePage homePage;
+    private LoginPage loginPage;
+    private CreacionCuenta creacionCuenta;
+
+    private String urlSitio;
+    private ArrayList<String> dataCP;
+    private String path;
+    private String browser;
+    private String propertyDriver;
+
+
+    //Métodos
+    @BeforeTest
+    public void preparacionEjecucion(){
+        path = Paths.get(System.getProperty("user.dir"), PropertiesDriven.getProperty("rutaDriver")).toString();
+        browser = PropertiesDriven.getProperty("browser");
+        propertyDriver = PropertiesDriven.getProperty("propertyDriver");
+        homePage = new HomePage(driver);
+        homePage.conexionBrowser(browser,path,propertyDriver);
+        loginPage = new LoginPage(homePage.getDriver());
+        creacionCuenta = new CreacionCuenta(homePage.getDriver());
+        dataCP = new ArrayList<>();
+    }
+
+    @BeforeMethod
+    public void preTests(){
+        urlSitio = PropertiesDriven.getProperty("url");
+        homePage.cargarSitio(urlSitio);
+        homePage.maximizarBrowser();
+    }
+
+    @AfterTest
+    public void posEjecucion(){
+        homePage.cerrarBrowser();
+    }
+
+    @Test
+    public void CP005_Correo_Registrado(){
+        dataCP = DataDriven.getData("CP005_Correo_Registrado");
+        homePage.irAIniciarSesion();
+        loginPage.esperarXSegundos(3000);
+        loginPage.crearCuenta();
+        creacionCuenta.creacionCuenta(dataCP.get(1),dataCP.get(2),dataCP.get(3),dataCP.get(4),dataCP.get(5),dataCP.get(6),dataCP.get(7),dataCP.get(8));
+        Assert.assertEquals(creacionCuenta.failedCuenta(), dataCP.get(9));
+    }
+
+}
